b557091ecfc25012ea0a9ba2764388643215dd06
Stela Trencheva <s.trencheva@student.fontys.nl>
2022-01-12 16:56:13 +0200

    cypress test refactor

--- budgetsimple_front_end/react-hooks-crud/cypress/integration/chnageWalletBudget.js
+++ budgetsimple_front_end/react-hooks-crud/cypress/integration/chnageWalletBudget.js
@@ -10,11 +10,15 @@ describe('Change Wallet Budget', () => {
 
         cy.visit('http://localhost:3000/user/wallets');
         cy.get(':nth-child(1) > .card-body > .row > :nth-child(1) > a > .smallLogo').click();
+        cy.wait(1000);
         cy.get(':nth-child(2) > .btn').click();
+        cy.wait(1000);
         cy.get('#newBudget').clear();
         cy.get('#newBudget').type('10');
         cy.get('form > .row > :nth-child(2) > .btn').click();
+        cy.wait(1000);
         cy.get(':nth-child(2) > .btn').click();
+        cy.wait(1000);
         cy.get(':nth-child(3) > :nth-child(2) > p').should('have.text', '10');
     })
 
