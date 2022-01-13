01db9359c65c7a60f18a65acb4e224485f9b34a3
Stela Trencheva <s.trencheva@student.fontys.nl>
2022-01-13 14:49:52 +0200

    Fix cypress tests

--- budgetsimple_front_end/react-hooks-crud/cypress/integration/seeAllRatings.js
+++ budgetsimple_front_end/react-hooks-crud/cypress/integration/seeAllRatings.js
@@ -10,11 +10,11 @@ describe('Rate the Application', () => {
 
         cy.visit('http://localhost:3000/allRatings');
         cy.get(':nth-child(1) > .card-body > .row > :nth-child(1) > a > .smallLogo').click();
-        cy.get(':nth-child(1) > .col-lg-12 > :nth-child(1)').should('have.text', 'Question: What do you like most about our website?');
-        cy.get(':nth-child(2) > .col-lg-12 > :nth-child(1)').should('have.text', 'Question: How often do you visit and use BudgetSimple?');
-        cy.get(':nth-child(3) > .col-lg-12 > :nth-child(1)').should('have.text', 'Question: Do you have any advice for us? Write it here!');
-        cy.get(':nth-child(4) > .col-lg-12 > :nth-child(1)').should('have.text', 'Question: On a scale of 1-10, how likely are you to recommend our website to your friends or family?');
-        cy.get(':nth-child(5) > .col-lg-12 > :nth-child(1)').should('have.text', 'Question: Did you face any challenge while using our website?');
+        cy.contains('Question: What do you like most about our website?').should('be.visible')
+        cy.contains('How often do you visit and use BudgetSimple?').should('be.visible')
+        cy.contains('Question: Do you have any advice for us? Write it here!').should('be.visible')
+        cy.contains('Question: On a scale of 1-10, how likely are you to recommend our website to your friends or family?').should('be.visible')
+        cy.contains('Question: Did you face any challenge while using our website?').should('be.visible')
         
     })
 
